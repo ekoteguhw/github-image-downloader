b1eb991a4b52fa5d9de0d136239162e4a8250171
Eko Teguh Widodo <id.ekoteguhw@gmail.com>
2019-08-16 11:19:28 +0700

    Update nama file langsung dari URLnya

--- downloader.py
+++ downloader.py
@@ -3,6 +3,9 @@ import requests
 url = 'https://avatars1.githubusercontent.com/u/835315?s=460&v=4'
 r = requests.get(url)
 
-f = open('ekoteguhw.jpg', 'wb')
+filename = url.split('/')[-1]
+new_filename = filename.split('?')[0] + '.jpg'
+
+f = open(new_filename, 'wb')
 f.write(r.content)
 f.close()
